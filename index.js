7b03d2f84e80d4aaf997c0ff76016e4c3ede50ca
Mara <125482894+Mara2403@users.noreply.github.com>
2023-09-08 12:37:46 +0200

    add function createGrid(rows, columns)

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-/*add eventListener to a button*/
+const sketchDiv = document.querySelector(".sketch");
+const singleCell = document.querySelector(".single-cell");
 const buttonColor = document.querySelector(".btn-color");
+const userInputValue = document.querySelector(".btn-size");
+
+
+function createGrid(rows, columns) {
+
+
+
+  
+  for (i = 0; i < rows * columns; i++) {
+
+  let singleCell = document.createElement("div");
+  singleCell.classList.add("single-cell");
+  
+  sketchDiv.appendChild(singleCell);
+}
+}
+
+
+createGrid(16, 16);
+
+
+
+
+
+
+
+/*add eventListener to a button*/
 buttonColor.addEventListener("click", colorChoice)
+
+
+
+
 
 function colorChoice() {
   console.log("clicked")
@@ -22,20 +54,33 @@ function addElement() {
 */
 
 
-document.body.onload = createDivs;
+document.body.onload = createGrid;
 
-function createDivs(num) {
- num = prompt("Enter a value", "0");
-  console.log(num);
+function createGridOnSize() {
+/* num = prompt("Enter a value", "0");
+  console.log(num); */
 
-for (let i = 0; i < num; i++) {
-
-  const newDiv = document.createElement("div");
-  const content = document.createTextNode("hi");
-  newDiv.appendChild(content);
-
-  const sketchDiv = document.getElementById("sketch");
-  sketchDiv.appendChild(newDiv);
+  userInputValue.addEventListener("click", changeSize);
+  
+  
+ 
+  
+    /*"input", () => {
+    rows = userInputValue.value;
+    columns = userInputValue.value; 
+  });*/
+    
 }
+
+/*
 };
+*/
+function changeSize() {
+  let userChoice = prompt("Enter a value", "0")
+  let rows = userChoice;
+  let columns = userChoice;
+  console.log(rows);
+  console.log(columns);
+}
+
 
