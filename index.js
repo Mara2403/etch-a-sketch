52265dab08e4bba24fe2c54437e2456263dbbdb9
Mara <125482894+Mara2403@users.noreply.github.com>
2023-09-09 12:48:08 +0200

    add resetGrid()

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const sketchDiv = document.querySelector(".sketch");
 const singleCell = document.querySelector(".single-cell");
 const buttonColor = document.querySelector(".btn-color");
 const userInputValue = document.querySelector(".btn-size");
+const resetButton = document.querySelector(".btn-reset");
 
 
 function createGrid(rows, columns) {
 
 
-
-  
   for (i = 0; i < rows * columns; i++) {
 
   let singleCell = document.createElement("div");
   singleCell.classList.add("single-cell");
   
   sketchDiv.appendChild(singleCell);
+
+  singleCell.addEventListener("mouseenter", (event) => event.target.style.backgroundColor = "black");
 }
 }
 
@@ -22,13 +23,39 @@ function createGrid(rows, columns) {
 createGrid(16, 16);
 
 
+function resetGrid() {
+  resetButton.addEventListener("click", function() {
+    console.log("clicked");
+    const singleCell = document.querySelectorAll(".single-cell");
+    console.log(singleCell);
+    singleCell.forEach(
+      function(node, index) {
+      node.style.backgroundColor = "beige";
+    }
+    );
+})
+};
+
+resetGrid();
 
 
 
+function changeSize() {
+  let userChoice = prompt("Enter a value", "0")
+console.log(userChoice);
+}
 
 
-/*add eventListener to a button*/
-buttonColor.addEventListener("click", colorChoice)
+
+function createGridOnSize() {
+
+  userInputValue.addEventListener("click", changeSize);
+let newNumberOfCells = 
+    singleCell.style.flexBasis = newNumberOfCells
+  }
+
+/* createGridOnSize(); */
+
 
 
 
@@ -37,6 +64,10 @@ buttonColor.addEventListener("click", colorChoice)
 function colorChoice() {
   console.log("clicked")
 }
+
+/*add eventListener to a button*/
+buttonColor.addEventListener("click", colorChoice)
+
 
 /* CREATE ONE DIV AND APPEND IT TO HTML INSIDE SKETCH DIV
 document.body.onload = addElement;
@@ -53,34 +84,9 @@ function addElement() {
 };
 */
 
-
-document.body.onload = createGrid;
-
-function createGridOnSize() {
-/* num = prompt("Enter a value", "0");
-  console.log(num); */
-
-  userInputValue.addEventListener("click", changeSize);
-  
-  
- 
-  
-    /*"input", () => {
-    rows = userInputValue.value;
-    columns = userInputValue.value; 
-  });*/
-    
-}
-
 /*
-};
+document.body.onload = createGrid;
 */
-function changeSize() {
-  let userChoice = prompt("Enter a value", "0")
-  let rows = userChoice;
-  let columns = userChoice;
-  console.log(rows);
-  console.log(columns);
-}
+
 
 
